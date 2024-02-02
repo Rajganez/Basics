9fdebe6f26f6e640082510245bdf68e864a329ad
Rajganez <ganezsouth@live.com>
2024-02-02 15:57:54 +0530

    line comment

--- Minimum value in an array.js
+++ Minimum value in an array.js
@@ -14,6 +14,8 @@ inp.on("line", (data) => {
 inp.on("close", () => {
   //start-here
   //Your code goes here … replace the below line with your code logic  */
+
+  // Using first index of an array finding the minimum value in an array
 let a = userInput[0];
 let b = a.split(" ");
 let arr=[];
@@ -21,26 +23,26 @@ let result = "";
 let fin = "";
 let i = 0;
 for (let j = 0 ; j<10 ; j++){
- arr.push(+b[j]);   
+ arr.push(+b[j]); //Pusing input from user into an array of 10 index   
 }
 while (i<=arr.length){
   i++;
- let temp = arr[0];
-  let temp1 = arr[i];
-if (temp === temp1){
+ let temp = arr[0]; //Assaining the first element of the array
+  let temp1 = arr[i]; //Assigning the elements through iterating the array
+if (temp === temp1){ // If the value of the array is same as [1,1,1,...]
     fin = temp;
     result = temp1;
 }
-else if (temp > temp1){
+else if (temp > temp1){ // assign minimum value in result variable
   result = temp1;
 }
-else if(result < temp){
+else if(result < temp){ // assing minimum value in fin variable if the first element is minimum
  fin = result; 
 }
 }
-if(fin < result){
+if(fin < result){ // display the result
  console.log(fin); 
-}else{
+}else{            // display the result
 console.log(result);
 }
   //end-here
